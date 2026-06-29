33f8f9b5a46167d426c893086d59823508bb5329
kalit1998shoh <kalit1998shoh@gmail.com>
2026-06-29 15:45:52 +0500

    Create menu.py

---
+++ keyboards/menu.py
@@ -0,0 +1,19 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="🎮 Public o'yin"),
+            KeyboardButton(text="🔒 Private Club"),
+        ],
+        [
+            KeyboardButton(text="👤 Profil"),
+            KeyboardButton(text="🏆 Reyting"),
+        ],
+        [
+            KeyboardButton(text="📖 Qoidalar"),
+            KeyboardButton(text="⚙️ Sozlamalar"),
+        ],
+    ],
+    resize_keyboard=True,
+)
