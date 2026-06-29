1cae9fafd4e8023fdd5f86048d71e7d8fc4856b4
kalit1998shoh <kalit1998shoh@gmail.com>
2026-06-29 22:27:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+
 from config import BOT_TOKEN
 
 from handlers.start import router as start_router
 from handlers.menu import router as menu_router
+from handlers.public import router as public_router
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 # Routerlarni ulash
 dp.include_router(start_router)
 dp.include_router(menu_router)
+dp.include_router(public_router)
 
 
 async def main():
