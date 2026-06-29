13a05527dc2029c5d50bd6b96d9d76e26027a6e5
kalit1998shoh <kalit1998shoh@gmail.com>
2026-06-29 22:47:00 +0500

    Update public.py

--- handlers/public.py
+++ handlers/public.py
@@ -2,34 +2,37 @@ from aiogram import Router, F
 from aiogram.types import Message
 
 from keyboards.public import public_menu
+import game
 
 router = Router()
-
-players = set()
 
 
 @router.message(F.text == "🎮 Public o'yin")
 async def public_game(message: Message):
 
-    if message.from_user.id in players:
+    if game.game_started:
+        await message.answer("🚫 O'yin allaqachon boshlangan.")
+        return
+
+    if message.from_user.id in game.players:
         await message.answer(
             f"✅ Siz allaqachon Lobbydasiz.\n\n"
-            f"👥 O'yinchilar soni: {len(players)}"
+            f"👥 O'yinchilar: {len(game.players)}"
         )
         return
 
-    players.add(message.from_user.id)
+    game.players.add(message.from_user.id)
 
-    if len(players) >= 4:
+    if len(game.players) >= 4:
         await message.answer(
             f"🎮 Public Lobby\n\n"
-            f"👥 O'yinchilar soni: {len(players)}\n\n"
-            f"✅ Endi o'yinni boshlash mumkin.",
+            f"👥 O'yinchilar: {len(game.players)}\n\n"
+            f"▶️ Endi o'yinni boshlash mumkin.",
             reply_markup=public_menu
         )
     else:
         await message.answer(
             f"🎮 Public Lobby\n\n"
-            f"👥 O'yinchilar soni: {len(players)}\n\n"
+            f"👥 O'yinchilar: {len(game.players)}\n\n"
             f"⏳ Kamida 4 ta o'yinchi kerak."
         )
