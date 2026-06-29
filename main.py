28d7df8135f87ab44975c36ea728f504e5e3052e
kalit1998shoh <kalit1998shoh@gmail.com>
2026-06-29 21:24:20 +0500

    Fix main router

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from handlers.start import router
+
+from handlers.start import router as start_router
+from handlers.menu import router as menu_router
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-dp.include_router(router)
+# Routerlarni ulash
+dp.include_router(start_router)
+dp.include_router(menu_router)
 
 
 async def main():
@@ -19,9 +23,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-from handlers.start import router
-from handlers.menu import router as menu_routerp
-
-
-dp.include_router(router)
-dp.include_router(menu_router)
