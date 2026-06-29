3beb941a8fce6f632b1ed5c3235eaa8a58f4424e
kalit1998shoh <kalit1998shoh@gmail.com>
2026-06-30 00:30:26 +0500

    Update night.py

--- keyboards/night.py
+++ keyboards/night.py
@@ -4,15 +4,16 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def night_keyboard(players):
     keyboard = []
 
-    for player in players:
-        keyboard.append(
-            [
-                InlineKeyboardButton(
-                    text=player["name"],
-                    callback_data=f"night_{player['id']}"
-                )
-            ]
-        )
+    for player_id, data in players.items():
+        if data["alive"]:
+            keyboard.append(
+                [
+                    InlineKeyboardButton(
+                        text=data["name"],
+                        callback_data=f"night_{player_id}"
+                    )
+                ]
+            )
 
     return InlineKeyboardMarkup(
         inline_keyboard=keyboard
