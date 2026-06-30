5e194581a3a34018cd9887c917278efe21dc0122
kalit1998shoh <kalit1998shoh@gmail.com>
2026-06-30 14:04:18 +0500

    Update game.py

--- game.py
+++ game.py
@@ -24,3 +24,54 @@ doctor_last_save = None
 
 # Ovoz berish
 votes = {}
+def check_winner():
+    mafia_count = 0
+    citizen_count = 0
+
+    for player_id in alive_players:
+        role = roles.get(player_id)
+
+        if role == "mafia":
+            mafia_count += 1
+        else:
+            citizen_count += 1
+
+    if mafia_count == 0:
+        return "citizens"
+
+    if mafia_count >= citizen_count:
+        return "mafia"
+
+    return None
+  
+  def reset_game():
+    global game_started
+    global day
+    global phase
+    global mafia_target
+    global doctor_save
+    global commissioner_check
+    global commissioner_action
+    global commissioner_shot
+    global commissioner_used_shot
+    global maniac_target
+    global doctor_last_save
+
+    players.clear()
+    roles.clear()
+    alive_players.clear()
+    dead_players.clear()
+    votes.clear()
+
+    game_started = False
+    day = 1
+    phase = "lobby"
+
+    mafia_target = None
+    doctor_save = None
+    commissioner_check = None
+    commissioner_action = None
+    commissioner_shot = None
+    commissioner_used_shot = False
+    maniac_target = None
+    doctor_last_save = None
