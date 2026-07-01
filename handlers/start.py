22bb2eec5e57dbcb41e770bbc5ccbb6d3ae9e36f
kalit1998shoh <kalit1998shoh@gmail.com>
2026-07-01 12:58:50 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,10 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
 
 from keyboards.menu import main_menu
 
@@ -9,8 +13,27 @@ router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message):
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="👥 O'yin guruhiga o'tish",
+                    url="https://t.me/+GPftnv8c3780YjYy"
+                )
+            ]
+        ]
+    )
+
     await message.answer(
         "🎮 Mafiya98 ga xush kelibsiz!\n\n"
-        "Quyidagi menyudan kerakli bo'limni tanlang:",
+        "🎭 Rollar va maxfiy vazifalar shu bot orqali yuboriladi.\n"
+        "💬 Muhokama va ovoz berish esa guruhda bo'ladi.\n\n"
+        "👇 Avval guruhga o'ting.",
+        reply_markup=keyboard
+    )
+
+    await message.answer(
+        "Kerakli bo'limni tanlang:",
         reply_markup=main_menu
     )
